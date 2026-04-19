73d6e41b644250af7634bc50119504b278a9e5b1
Ayaan Buksh <ac159208@avcol.school.nz>
2026-04-19 20:35:23 +1200

    dont know will fix later

--- NBApp/Program.cs
+++ NBApp/Program.cs
@@ -1,40 +1,44 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NBApp.Areas.Identity.Data;
 using NBApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("NBAppContextConnection") ?? throw new InvalidOperationException("Connection string 'NBAppContextConnection' not found.");;
 
-builder.Services.AddDbContext<NBAppContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<NBAppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<NBAppContext>();
+builder.Services.AddDbContext<NBAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
+//DI for DbContext
+builder.Services.AddDefaultIdentity<NBAppUser>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<NBAppContext>();
+
+
+// Add Session State with server memory (distributed memory cache)
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 builder.Services.AddControllersWithViews();
 
+// This automatically scans your project and registers all validators it finds
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseSession(); // Enable Session State middleware
 
-// Configure the HTTP request pipeline.  
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapDefaultControllerRoute();
+app.MapRazorPages(); // Required for Identity UI
 
 app.Run();
