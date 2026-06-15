2b8ad225d15aabc419e8bc1420f066c2bd61f737
Ayaan Buksh <ac159208@avcol.school.nz>
2026-06-15 20:13:22 +1200

    added stripe and External login, Dunno about MPaisa Intergration but now have to look for problems ig

--- NBApp/Models/NBAppUser.cs
+++ NBApp/Models/NBAppUser.cs
@@ -14,7 +14,7 @@ public class NBAppUser : IdentityUser
     [RegularExpression(@"[a-zA-Z0-9]*$",ErrorMessage ="No spaces or special characters")]
     public required string DisplayName { get; set; }//the name that will be displayed for the user in the application, which can be different from their email.
 
-    public string? ProfilePicturePath { get; set; } = "/Images/ProfilePictures/default.png";// profile picture is up to interpretation. no further explanation
+    public string? ProfilePicturePath { get; set; } // profile picture is up to interpretation. no further explanation
 
     public List<Order> Orders { get; set; } = new();// creates a one-to-many relationship between NBAppUser and Order, where one user can have multiple orders.
 
--- NBApp/Program.cs
+++ NBApp/Program.cs
@@ -17,6 +17,18 @@ builder.Services.AddDefaultIdentity<NBAppUser>(options =>
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<NBAppContext>();
 
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+    })
+    .AddFacebook(options =>
+    {
+        options.AppId = builder.Configuration["Authentication:Facebook:AppId"]!;
+        options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"]!;
+    });
+
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 builder.Services.AddDistributedMemoryCache();
