c09f9b25b153806c742e9307f1956c2c7b3842f8
Ayaan Buksh <ac159208@avcol.school.nz>
2026-08-15 15:58:34 +1200

    fixed gitignore and anonymized admin details

--- NBApp/Models/IdentityConfig.cs
+++ NBApp/Models/IdentityConfig.cs
@@ -1,40 +1,42 @@
-﻿
-    using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
-    namespace NBApp.Models
+namespace NBApp.Models
+{
+    public class IdentityConfig
     {
-        public class IdentityConfig
+        public static async Task CreateAdminUserAsync(IServiceProvider provider)
         {
-            public static async Task CreateAdminUserAsync(IServiceProvider provider)
+            var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = provider.GetRequiredService<UserManager<NBAppUser>>();
+            var config = provider.GetRequiredService<IConfiguration>();
+
+            string username = config["AdminUser:Email"]!;
+            string password = config["AdminUser:Password"]!;
+            string roleName = "Admin";
+
+            if (await roleManager.FindByNameAsync(roleName) == null)
             {
-                var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
-                var userManager = provider.GetRequiredService<UserManager<NBAppUser>>();
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
 
-                string username = "admin@DaGoat.com";
-                string password = "MessiDaGoat10!";
-                string roleName = "Admin";
+            if (await userManager.FindByNameAsync(username) == null)
+            {
+                NBAppUser user = new()
+                {
+                    UserName = username,
+                    Email = username,
+                    EmailConfirmed = true,
+                    DisplayName = "Admin",
+                    ProfilePicturePath = "/Images/ProfilePictures/AdminPfp.png"
+                };
 
-                // Check if the role exists, if not, create it
-                if (await roleManager.FindByNameAsync(roleName) == null)
+                var result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
                 {
-                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                    await userManager.AddToRoleAsync(user, roleName);
                 }
-                if(await userManager.FindByNameAsync(username) == null)
-                {
-                    NBAppUser user = new () 
-                    { 
-                        UserName = username,
-                        Email = "admin@DaGoat.com",
-                        EmailConfirmed = true,
-                        DisplayName = "Admin" ,
-                        ProfilePicturePath = "/Images/ProfilePictures/AdminPfp.png"
-                    };
-                    var result = await userManager.CreateAsync(user, password);
-                    if(result.Succeeded)
-                    {
-                        await userManager.AddToRoleAsync(user, roleName);
-                    }
-                }
-            }   
+            }
         }
     }
+}
